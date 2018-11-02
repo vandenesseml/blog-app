5a66d58c1696b727e4ab382bd833e5b2815addba
Max <vandenesseml@vcu.edu>
2018-11-02 01:29:02 -0400

    update

--- app/routes.py
+++ app/routes.py
@@ -149,7 +149,7 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('latest'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -159,7 +159,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('latest')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
@@ -167,7 +167,7 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('latest'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -249,7 +249,10 @@ def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('latest')
+        return redirect(next_page)
     if user == current_user:
         flash('You cannot follow yourself!')
         return redirect(url_for('user', username=username))
@@ -265,7 +268,10 @@ def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('latest')
+        return redirect(next_page)
     if user == current_user:
         flash('You cannot unfollow yourself!')
         return redirect(url_for('user', username=username))
